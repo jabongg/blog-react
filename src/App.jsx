18315eeabe65510105e92a416561db3c5bf8e2f1
EPAM\jangbahadur_patel <jangbahadur_patel@epam.com>
2025-02-07 19:18:16 +0530

    appwrite: first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+
+  return (
+    <>
+      <h1>A blog with writeapp</h1>
+    </>
+  )
+}
+
+export default App
